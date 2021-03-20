3ca455ff00c278828d83d51311dc806ee962e3ff
Mikkel-KR <Mikkel.kora@gmail.com>
2021-03-20 19:39:58 +0100

    Tilføjet newtonsoft.json nuget-pakke og swagger nuget-pakke.

--- F21DISP.Delprojekt.Gruppe6.Microservice/Backend/Startup.cs
+++ F21DISP.Delprojekt.Gruppe6.Microservice/Backend/Startup.cs
@@ -1,17 +1,11 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Backend.Data;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.OpenApi.Models;
 
 namespace Backend
 {
@@ -29,6 +23,16 @@ namespace Backend
         {
             services.AddControllers();
 
+            // Register the Swagger generator, defining 1 or more Swagger documents
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "My API", Version = "v1" });
+            });
+            //    Cycles in Domaine model
+            //    https://stackoverflow.com/questions/58846765/cycle-in-database
+            //https://stackoverflow.com/questions/55787018/net-core-3-preview-4-addnewtonsoftjson-is-not-defined
+            services.AddControllers().AddNewtonsoftJson(x => x.SerializerSettings.ReferenceLoopHandling = Newtonsoft.Json.ReferenceLoopHandling.Ignore);
+
             services.AddDbContext<ApplicationDbContext>(options =>
                 options.UseSqlServer(Configuration.GetConnectionString("DockerConnection")));
         }
@@ -43,6 +47,17 @@ namespace Backend
 
             //Do "Update-Database" runtime. Requires an "Add-Migration" done first
             db.Database.Migrate();
+
+            // Enable middleware to serve generated Swagger as a JSON endpoint.
+            app.UseSwagger();
+
+            // Enable middleware to serve swagger-ui (HTML, JS, CSS, etc.),
+            // specifying the Swagger JSON endpoint.
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
+                c.RoutePrefix = string.Empty; //Hint set swagger in root!
+            });
 
             app.UseHttpsRedirection();
 
