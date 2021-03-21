acd16eb9c8874709a4574aa3b9ef4bac6adf5fb9
Mikkel-KR <Mikkel.kora@gmail.com>
2021-03-21 13:57:52 +0100

    Removed home-controller links (+ root er nu haandvaerker)

--- F21DISP.Delprojekt.Gruppe6.Microservice/Frontend/Startup.cs
+++ F21DISP.Delprojekt.Gruppe6.Microservice/Frontend/Startup.cs
@@ -72,7 +72,7 @@ namespace Frontend
             {
                 endpoints.MapControllerRoute(
                     name: "default",
-                    pattern: "{controller=Home}/{action=Index}/{id?}");
+                    pattern: "{controller=Haandvaerker}/{action=Index}/{id?}");
             });
         }
     }
